75ef24453498b580cc59b6caecdcba2e1b97a849
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-01-20 11:07:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+    document.getElementById("p").innerHTML=document.getElementById("name").value
     render() {
         return ("<input placeholder='YOUR NAME' id='name'><p id='p'>");
-        document.getElementById("p").innerHTML=document.getElementById("name").value
     }
 }
 
